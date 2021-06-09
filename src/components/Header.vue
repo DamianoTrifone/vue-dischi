0b637fdadf8039a05af6e0736ebb3b92f48ebd4e
DamianoTrifone <trifone.damiano@gmail.com>
2021-06-09 15:16:30 +0200

    Barra superiore

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="barra">
+      <img class="logo_header" src="../assets/spotify.png" alt="Logo spotify">
+  </section>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style>
+    .barra{
+        background-color: #1E2D3B;
+        height: 65px;
+    }
+
+    .logo_header{
+        width: 45px;
+        margin-left: 10px;
+        margin-top: 10px;
+    }
+</style>
