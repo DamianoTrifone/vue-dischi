697840d645b27c37b6425d22fe5f6c27969d2f4d
DamianoTrifone <trifone.damiano@gmail.com>
2021-06-10 16:24:39 +0200

    Filtro generi

--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,28 +1,32 @@
 <template>
   <div class="copertine">
-    <div
-        class="card_container"
-        v-for="(album, index) in albums" 
-        :key="index"
-    >
-        <Album :item="album" />
-    </div>
+        <Search @selectGender="onSelectGender" />
+        <div
+            class="card_container"
+            v-for="(album, index) in filteredAlbums" 
+            :key="index"
+        >
+            <Album :item="album" />
+        </div>
   </div>
 </template>
  
 <script>
 import axios from 'axios';
 import Album from "./Album.vue";
+import Search from "./Search.vue";
 
 export default {
     name: "Albums",
     components: {
-        Album
+        Album,
+        Search
     },
     data: function () {
         return {
             apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
             albums: [],
+            genre: 'all'
         }
     },
     created: function(){
@@ -30,8 +34,25 @@ export default {
             .get(this.apiUrl)
             .then((response) => {
                 this.albums = response.data.response;
-                console.log(this.albums);
             })
+    },
+
+    methods: {
+        onSelectGender: function(payload){
+            this.genre = payload;
+        }
+    },
+
+    computed: {
+        filteredAlbums: function (){
+            if (this.genre !== 'all') {
+                return this.albums.filter(album =>{
+                    return album.genre.toLowerCase().includes(this.genre.toLowerCase())
+                })
+            } else {
+                return this.albums;
+            }
+        }
     }
 }
 </script>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,14 +1,20 @@
 <template>
-  <Search />
+    <select name="genre" id="genre" v-model="selected" @change="$emit('selectGender', selected)">
+        <option disabled>Seleziona il genere</option>
+        <option value="all">Tutti</option>
+        <option value="pop">Pop</option>
+        <option value="rock">Rock</option>
+        <option value="jazz">Jazz</option>
+        <option value="metal">Metal</option>
+    </select>
 </template>
 
 <script>
-import Search from './Search.vue';
 export default {
-    name: "Search"
+    data() {
+        return {
+            selected: ''
+        }
+    }
 }
 </script>
-
-<style>
-
-</style>
